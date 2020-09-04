374a243e43f658ee21c41b24441c80d92465e620
HieuTrinh <hieu.trinh@student.tut.fi>
2020-09-04 09:10:19 +0300

    add Dao and Database

--- app/src/main/java/com/example/paging/events/db/EventDao.kt
+++ app/src/main/java/com/example/paging/events/db/EventDao.kt
@@ -1,4 +1,20 @@
 package com.example.paging.events.db
 
-class EventDao {
+import androidx.paging.PagingSource
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.paging.events.model.Event
+
+interface EventDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(event: List<Event>)
+
+    @Query("SELECT * FROM event WHERE " +
+    "nameEn LIKE :queryString OR descFull LIKE :queryString OR descShort LIKE :queryString OR addStreetAddress LIKE :queryString " +
+    "ORDER BY startingDay ASC, nameEn ASC")
+    fun eventByName(queryString: String): PagingSource<Int, Event>
+
+    @Query("DELETE FROM event")
+    suspend fun clearEvent()
 }
--- app/src/main/java/com/example/paging/events/db/EventDatabase.kt
+++ app/src/main/java/com/example/paging/events/db/EventDatabase.kt
@@ -1,4 +1,34 @@
 package com.example.paging.events.db
 
-class EventDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [DatabaseEvent::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class EventDatabase : RoomDatabase() {
+
+    abstract fun eventDao(): EventDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: EventDatabase? = null
+
+        fun getInstance(context: Context): EventDatabase =
+            INSTANCE
+                ?: synchronized(this) {
+                    INSTANCE
+                        ?: buildDatabase(context).also { INSTANCE = it }
+                }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                EventDatabase::class.java, "Event.db")
+                .build()
+    }
 }
