f986b633eca170759bcade152fcc0ffe0856666c
HieuTrinh <hieu.trinh@student.tut.fi>
2020-12-08 07:12:32 +0200

    add guidance

--- app/src/main/java/com/example/paging/events/data/EventRepository.kt
+++ app/src/main/java/com/example/paging/events/data/EventRepository.kt
@@ -4,4 +4,7 @@ class EventRepository(
 //    private val service: GithubService,
 //    private val database:
 ) {
+    // Viet function de convert response thanh database object trong app
+    // Viet function de convert database object thanh data trong app (domain)
+    // can con: datasource, remote mediator, repository
 }
